a3df711327105177f60e1399c8e344be7155de3f
atulmy <atul.12788@gmail.com>
2015-08-02 03:34:24 +0530

    Android settings

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+    name: 'Rock Paper Scissors',
+    description: 'Play Rock Paper Scissors online with your friends or A.I. (computer)!',
+    version: '0.0.1'
+});
+
+App.icons({
+    'android_ldpi': 'resources/icons/icon-ldpi.png',
+    'android_mdpi': 'resources/icons/icon-mdpi.png',
+    'android_hdpi': 'resources/icons/icon-hdpi.png',
+    'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
